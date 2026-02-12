11b9d1fc3aa59499e0be23b1de0029c7505dabf1
manav girnara <manavgirnara@gmail.com>
2026-02-12 13:44:38 +0530

    changes are added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -79,6 +79,7 @@ const Register = () => {
   };
 
   return (
+    <div className="register-page">
     <div className="form-container">
       <h1 className="form-title">CREATE ACCOUNT</h1>
       <form onSubmit={handleSubmit}>
@@ -187,6 +188,7 @@ const Register = () => {
       <p className="link-text">
         Already have an account? <a href="/Login">Login here</a>
       </p>
+    </div>
     </div>
   );
 };
